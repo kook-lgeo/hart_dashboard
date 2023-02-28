75e999ef2e4e36874a811ea4e03f69ae215963c9
kook-lgeo <kook@lgeo.co>
2023-02-28 14:10:27 -0800

    Fix - file name change

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
+# Import necessary libraries 
+from dash import html, dcc
+from dash.dependencies import Input, Output
 
-# Define the Dash App and it's attributes here 
+# Connect to main app_file.py file
+from app_file import app
 
-import dash
-import dash_bootstrap_components as dbc
+# Connect to your app pages
+from pages import page1, page2, page3
 
-app = dash.Dash(__name__, 
-                external_stylesheets=[dbc.themes.BOOTSTRAP], 
-                meta_tags=[{"name": "viewport", "content": "width=device-width"}],
-                suppress_callback_exceptions=True)
+# Connect the navbar to the index
+from components import navbar
+
+# Define the navbar
+nav = navbar.Navbar()
+
+# Define the index page layout
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    # nav, 
+    html.Div(id='page-content', children=[]), 
+])
+
+# Create the callback to handle mutlipage inputs
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/page1':
+        return page1.layout
+    if pathname == '/page2':
+        return page2.layout
+    if pathname == '/page3':
+        return page3.layout
+    else: # if redirected to unknown link
+        return "404 Page Error! Please choose a link" #page1.layout
+
+# Run the app on localhost:8050
+if __name__ == '__main__':
+    app.run_server(debug=True)
